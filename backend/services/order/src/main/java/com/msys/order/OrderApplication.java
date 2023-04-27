cacbd178e11f9030cf8d3db1bbb1ee5f4ba19476
RIAZ QUADRI <riazbls455@gmail.com>
2023-04-27 11:38:24 +0530

    initial

---
+++ backend/services/order/src/main/java/com/msys/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.msys.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
